f7ff2dc2f73b980071b3b91969b610e1e852efea
Arsalan Peerzada <peerzada_1994@live.com>
2019-03-24 08:13:02 +0500

    Update

---
+++ pattern/pattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number for pattern");
+            int lines = Convert.ToInt32(Console.ReadLine());
+            int spaces = lines-1;
+            int number = spaces-2;
+            int reverse = lines - spaces;
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(" ");                
+                }
+                spaces--;
+
+                for (int k = 1; k < number; k++)
+                {
+                    Console.Write(k);
+                }
+                number++;
+                for (int k = 1; k < reverse; k++)
+                {
+                    Console.Write(k);
+                }
+                reverse++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
